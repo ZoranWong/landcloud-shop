ab8f40cea0eb203aff9e2a0fff3f81faf572bfa4
ZoranWong <904110231@qq.com>
2019-04-27 19:17:25 +0800

    debug

--- extend/org/upload/driver/Local.php
+++ extend/org/upload/driver/Local.php
@@ -40,7 +40,7 @@ class Local
      */
     public function checkRootPath($rootpath)
     {
-        $rootpath = $rootpath ?: './static/uploads/';
+        $rootpath = $rootpath ?: '/static/uploads/';
         if (!(is_dir($rootpath) && is_writable($rootpath))) {
             $this->error = '上传根目录不存在！请尝试手动创建:' . $rootpath;
             return false;
