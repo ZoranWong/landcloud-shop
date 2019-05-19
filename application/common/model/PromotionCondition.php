b28931f08fd2d6f867a7e356a3dcc9e522284acf
ZoranWong <904110231@qq.com>
2019-05-19 14:23:12 +0800

    fix

--- application/common/model/PromotionCondition.php
+++ application/common/model/PromotionCondition.php
@@ -352,6 +352,7 @@ class PromotionCondition extends Common
                 return '所以商品';
             }
             case self::SOME_GOODS: {
+                var_dump($params);exit;
                 $ids = explode(',', $params['goods_id']);
                 $goods = Goods::whereIn('id', $ids)->select();
                 $str = '<ul>';
