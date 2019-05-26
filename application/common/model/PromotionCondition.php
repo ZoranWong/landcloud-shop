6e6df92244cd13d828f7a41aa210de2984c2831b
ZoranWong <904110231@qq.com>
2019-05-26 18:30:46 +0800

    fix

--- application/common/model/PromotionCondition.php
+++ application/common/model/PromotionCondition.php
@@ -114,6 +114,7 @@ class PromotionCondition extends Common
             if ($this->code[$conditionInfo['code']]['type'] == 'goods') {
                 $key = false;
                 foreach ($cart['list'] as $k => $v) {
+                    Log::debug('------------------- begin ----------------------');
                     $type = $this->$method($params, $v['product_id'], $v['nums']);
                     Log::debug('type = '.$type.' '.$v['product_id'].' '.$params['goods_id']);
                     if ($type > 0) {
