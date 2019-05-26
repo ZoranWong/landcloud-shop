b5729dbd0a754de213e4c204c334baebb72c57e3
ZoranWong <904110231@qq.com>
2019-05-26 16:52:54 +0800

    fix

--- application/common/model/Promotion.php
+++ application/common/model/Promotion.php
@@ -45,7 +45,7 @@ class Promotion extends Common
 
         $list = $this->where($where)->order('sort', 'asc')->select();
         foreach ($list as $v) {
-            $this->setPromotion($v, $cart, false);
+            $this->setPromotion($v, $cart);
             //如果排他，就跳出循环，不执行下面的促销了
             if ($v['exclusive'] == self::EXCLUSIVE_YES) {
                 break;
