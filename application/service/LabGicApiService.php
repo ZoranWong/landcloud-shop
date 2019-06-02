3bb89f2b9aff48ad4eb863e117971b2e777f34da
wong1989 <skyandsunning@163.com>
2019-06-02 15:10:15 +0800

    测试

--- application/service/LabGicApiService.php
+++ application/service/LabGicApiService.php
@@ -50,8 +50,9 @@ class LabGicApiService
         $method = '/api/api/GetCurrCreditSP';
         $id = urlencode(base64_encode(!is_array($id) ? $id : $id[0]));
         $code = md5($this->apiKey . $id);
-        $response = $this->http->get("{$this->host}{$method}", ['query' => ['CusCode' => $id, 'ApiKey' => $code]]);
-        Log::debug('-------------- '.$response->getBody()->getContents().' ------------');
+        $response = $this->http->get("{$this->host}{$method}", ['query' => ['CusCode' => $id, 'Apikey' => $code]]);
+        Log::debug('-------------- '.$response->getBody()->getContents()
+            .' ------------');
         $data = json_decode($response->getBody()->getContents(), true);
         if ($data['code'] === self::SUCCESS_CODE) {
             return $data['data'];
