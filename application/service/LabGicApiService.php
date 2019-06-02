b46e64f86f542b0e20c7ebad67577df7635795ff
wong1989 <skyandsunning@163.com>
2019-06-02 16:42:32 +0800

    测试

--- application/service/LabGicApiService.php
+++ application/service/LabGicApiService.php
@@ -53,7 +53,7 @@ class LabGicApiService
         $response = $this->http->get("{$this->host}{$method}", ['query' => ['CusCode' => $cusCode, 'Apikey' => $code]]);
         Log::debug('-------------- '.$response->getBody()->getContents() . ' ------------ '.json_encode(['CusCode' => $id, 'Apikey' => $code]));
         $data = json_decode($response->getBody()->getContents(), true);
-        if ($data['code'] === self::SUCCESS_CODE) {
+        if ($data['code'] == self::SUCCESS_CODE) {
             return $data['data'];
         } else {
             return 0;
