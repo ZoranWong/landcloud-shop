1037223a7d0832abba2060ddfed859873889930d
wong1989 <skyandsunning@163.com>
2019-06-21 03:34:50 +0800

    fix

--- application/manage/controller/Report.php
+++ application/manage/controller/Report.php
@@ -397,6 +397,7 @@ class Report extends Manage
             return $result;
         } else {
             $areas = \app\common\model\Area::where('parent_id', 'eq', '0')->select()->all();
+            var_dump($areas);
             return $this->fetch('visit_goods', ['areas' => $areas]);
         }
     }
