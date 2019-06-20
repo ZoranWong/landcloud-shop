d380ff9728156ee3aaaeb8d784f285e176fac169
wong1989 <skyandsunning@163.com>
2019-06-21 03:12:14 +0800

    fix

--- thinkphp/library/think/db/Query.php
+++ thinkphp/library/think/db/Query.php
@@ -3005,7 +3005,6 @@ class Query
         $this->options['data'] = $data;
 
         $resultSet = $this->connection->select($this);
-        var_dump($this->options);
         if ($this->options['fetch_sql']) {
             return $resultSet;
         }
