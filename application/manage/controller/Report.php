e48beebc6a09bdf8f50f64a83b86518c1547652d
wong1989 <skyandsunning@163.com>
2019-06-21 11:02:21 +0800

    fix

--- application/manage/controller/Report.php
+++ application/manage/controller/Report.php
@@ -383,6 +383,7 @@ class Report extends Manage
             $result['count'] = $query->count();
             $list = $result['count'] ? $query->fetchSql(false)->page($page)->limit($limit)->select() : [];
             foreach ($list as &$item) {
+                $item['product_sn'] = $item->product['sn'];
                 $item['area_name'] = $item->area['name'];
                 $item['product_name'] = $item->product['name'];
             }
