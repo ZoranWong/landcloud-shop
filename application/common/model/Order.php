951749299187a19ab8e19d2508ece84015731a3e
wong1989 <skyandsunning@163.com>
2019-06-21 16:44:04 +0800

    用户添加测试标示

--- application/common/model/Order.php
+++ application/common/model/Order.php
@@ -203,9 +203,9 @@ class Order extends Common
         $limit = $input['limit'] ? $input['limit'] : 20;
         $query = $this->alias('o');
         if (empty($input['super']) || !$input['super']) {
-            $query->has('user', function ($query) {
+            $query = self::has('user', function ($query) {
                 $query->where('is_tester', 'eq', false);
-            });
+            })->alias('o');
         }
         if (!empty($input['search'])) {
             $query->where(function ($query) use ($input) {
