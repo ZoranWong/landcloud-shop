052ceef47d9c510233795d547e44bafb4a21de57
wong1989 <skyandsunning@163.com>
2019-06-21 17:10:49 +0800

    用户添加测试标示

---
+++ database/migrations/20190621090916_add_is_special_to_goods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class AddIsSpecialToGoodsTable extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('goods')
+            ->addColumn(Column::boolean('is_special')->setDefault(0)
+                ->setComment('特殊价格产品（隐藏价）'))
+            ->update();
+    }
+}
