92999100bec794c5f7541eafa89cf32f065add6c
ZoranWong <904110231@qq.com>
2019-07-12 10:57:48 +0800

    fix

--- application/common/model/Order.php
+++ application/common/model/Order.php
@@ -892,8 +892,8 @@ class Order extends Common
     {
         $where[] = array('order_id', 'in', $id);
 //        $where[] = array('pay_status', 'eq', self::PAY_STATUS_NO);
-        $where[] = array('status', 'eq', self::ORDER_STATUS_NORMAL);
-        $where[] = array('ship_status', 'eq', self::SHIP_STATUS_NO);
+//        $where[] = array('status', 'eq', self::ORDER_STATUS_NORMAL);
+//        $where[] = array('ship_status', 'eq', self::SHIP_STATUS_NO);
 
         if ($user_id) {
             $where[] = array('user_id', 'eq', $user_id);
