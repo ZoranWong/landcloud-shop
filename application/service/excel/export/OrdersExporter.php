3c49ba58c570d6779b1acf0bc2fdf24233afcddd
ZoranWong <904110231@qq.com>
2019-07-22 20:06:06 +0800

    登录

--- application/service/excel/export/OrdersExporter.php
+++ application/service/excel/export/OrdersExporter.php
@@ -53,7 +53,7 @@ class OrdersExporter extends BaseGenerator
         }
         Log::info('-----------=========---------'.json_encode($filter));
         if (isset($filter['date'])) {
-            $dateString = $filter['date'];
+            $dateString = urldecode($filter['date']);
             $dateArray = explode('到', $dateString);
             $sDate = strtotime($dateArray[0] . ' 00:00:00');
             $eDate = strtotime($dateArray[1] . ' 23:59:59');
